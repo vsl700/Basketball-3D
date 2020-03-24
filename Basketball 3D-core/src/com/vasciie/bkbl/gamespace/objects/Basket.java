4d5996568a6c233ff24ca2583449217f8faca5fc
vsl700 <51147745+vsl700@users.noreply.github.com>
2020-03-24 16:58:42 +0200

    Took useless import out

--- Basketball 3D-core/src/com/vasciie/bkbl/gamespace/objects/Basket.java
+++ Basketball 3D-core/src/com/vasciie/bkbl/gamespace/objects/Basket.java
@@ -12,7 +12,6 @@ import com.badlogic.gdx.graphics.g3d.model.Node;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 import com.badlogic.gdx.graphics.g3d.utils.shapebuilders.BoxShapeBuilder;
 import com.badlogic.gdx.graphics.g3d.utils.shapebuilders.CylinderShapeBuilder;
-import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.bullet.collision.btBoxShape;
 import com.badlogic.gdx.physics.bullet.collision.btCollisionObject;
